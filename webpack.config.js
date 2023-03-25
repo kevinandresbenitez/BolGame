aae25b8affa24569d57c2a8ca7367f4286eb8d85
kevin Benitez <kevinescudoderoble@gmail.com>
2023-03-25 19:32:15 -0300

    Webpack base Proyect

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  mode:'production',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+
+  plugins: [new HtmlWebpackPlugin({title:"Three js Proyect",filename:'index.html'})],
+};
