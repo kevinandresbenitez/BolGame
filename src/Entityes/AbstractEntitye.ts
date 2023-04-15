71d4df9f506914ed9a91e445fe472dbf439a2ddd
kevin Benitez <kevinescudoderoble@gmail.com>
2023-04-15 10:32:53 -0300

    Init proyect bol game

---
+++ src/Entityes/AbstractEntitye.ts
@@ -0,0 +1,12 @@
+export class Entityes{
+    canUpdateLogic:boolean;
+
+    constructor(canUpdateLogic:boolean){
+        this.canUpdateLogic = canUpdateLogic ;
+    }
+
+
+    remove(){
+
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { Entityes } from "./Entityes/AbstractEntitye";
+
+console.log(new Entityes(true))
